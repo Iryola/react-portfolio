c01ab4fe32407be1b63ee659f0b73ff84d8c355f
Iryola <adamfam@hotmail.com>
2019-05-21 16:22:46 -0600

    Whole bunch of AUTH stuff

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 
-
-import NavigationContainer from './navigation/navigation-container'
+import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
@@ -11,34 +11,106 @@ import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    };
 
-export default class App extends Component {  
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+    this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
+    this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this);
+  }
+
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    });
+  }
+
+  handleUnsuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+
+  handleSuccessfulLogout() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+
+  checkLoginStatus() {
+    return axios
+      .get("https://api.devcamp.space/logged_in", { withCredentials: true })
+      .then(response => {
+        const loggedIn = response.data.logged_in;
+        const loggedInStatus = this.state.loggedInStatus;
+
+        if (loggedIn && loggedInStatus === "LOGGED_IN") {
+          return loggedIn;
+        } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "LOGGED_IN"
+          });
+        } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN"
+          });
+        }
+      })
+      .catch(error => {
+        console.log("Error", error);
+      });
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
+
+  authorizedPages() {
+    return [<Route path="/blog" component={Blog} />];
+  }
+
   render() {
     return (
-      <div className='container'>         
+      <div className="container">
         <Router>
-          <div>            
-            <NavigationContainer />
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/about-me" component={About} />
-                <Route path="/auth" component={Auth} />
-                <Route path="/contact" component={Contact} />
-                <Route path="/blog" component={Blog} />
-                <Route 
-                  exact
-                  path="/portfolio/:slug" 
-                  component={PortfolioDetail} />
-                <Route component={NoMatch} />
-
-              </Switch>
+          <div>
+            <NavigationContainer
+              loggedInStatus={this.state.loggedInStatus}
+              handleSuccessfulLogout={this.handleSuccessfulLogout}
+            />
+            <h2>{this.state.loggedInStatus}</h2>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              {this.state.loggedInStatus === "LOGGED_IN"
+                ? this.authorizedPages()
+                : null}
+              <Route
+                path="/auth"
+                render={props => (
+                  <Auth
+                    {...props}
+                    handleSuccessfulLogin={this.handleSuccessfulLogin}
+                    handleUnsuccessfulLogin={this.handleUnSuccessfulLogin}
+                  />
+                )}
+              />
+              <Route path="/contact" component={Contact} />
+              <Route
+                exact
+                path="/portfolio/:slug"
+                component={PortfolioDetail}
+              />
+              <Route component={NoMatch} />
+            </Switch>
           </div>
         </Router>
-
-        
       </div>
-      
     );
   }
 }
--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,22 +1,40 @@
-import React, { Component } from 'react';
-import Login from "../auth/login"
-import loginImg from "../../../static/assets/images/auth/login.jpg"
+import React, { Component } from "react";
+import Login from "../auth/login";
+import loginImg from "../../../static/assets/images/auth/login.jpg";
 
- export default class Auth extends Component {
-    render() {
-        return (
-            <div className="auth-page-wrapper">
-                <div className="left-column" 
-                style={{
-                    backgroundImage: `url(${loginImg})`
-                }}
-                />
+export default class Auth extends Component {
+  constructor(props) {
+    super(props);
 
-                <div className="right-column">
-                    <Login />                    
-                </div>
-            </div>
-        );
-    }
+    this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+    this.handleUnsuccessfulAuth = this.handleUnsuccessfulAuth.bind(this);
+  }
+
+  handleSuccessfulAuth() {
+    this.props.handleSuccessfulLogin();
+    this.props.history.push("/");
+  }
+
+  handleUnsuccessfulAuth() {
+    this.props.handleUnsuccessfulLogin();
+  }
+  render() {
+    return (
+      <div className="auth-page-wrapper">
+        <div
+          className="left-column"
+          style={{
+            backgroundImage: `url(${loginImg})`
+          }}
+        />
+
+        <div className="right-column">
+          <Login
+            handleSuccessfulAuth={this.handleSuccessfulAuth}
+            handleUnsuccessfulAuth={this.handleUnsuccessfulAuth}
+          />
+        </div>
+      </div>
+    );
+  }
 }
-
