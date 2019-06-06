e2e6d407eabb123f59a38dd75192ab26fed8b4af
Iryola <adamfam@hotmail.com>
2019-06-06 15:28:41 -0600

    working on the blog feature

--- src/components/app.js
+++ src/components/app.js
@@ -14,6 +14,7 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
+import BlogDetail from "./pages/blog-detail";
 import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
@@ -103,6 +104,7 @@ export default class App extends Component {
               <Route exact path="/" component={Home} />
               <Route path="/about-me" component={About} />
               <Route path="/blog" component={Blog} />
+              <Route path="/b/:slug" component={BlogDetail} />
               {this.state.loggedInStatus === "LOGGED_IN"
                 ? this.authorizedPages()
                 : null}
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,8 +1,68 @@
-import React from "react";
-import { Link } from "react-router-dom"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import BlogItem from "../blog/blog-item";
 
-export default function () {
+class Blog extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+    this.activateInfiniteScroll();
+  }
+
+  activateInfiniteScroll() {
+    window.onscroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        console.log("get more posts");
+      }
+    };
+  }
+
+  getBlogItems() {
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    });
+
+    axios
+      .get("https://stevenadams.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs,
+          totalCount: response.data.meta.total_records
+        });
+      })
+      .catch(error => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+    });
+
     return (
-    <div>Blog</div>
+      <div className="blog-container">
+        <div className="content-container">{blogRecords}</div>
+      </div>
     );
+  }
 }
+
+export default Blog;
